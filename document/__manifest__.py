24ce421b437bdf1e8802a2d3100e91dbc5cd02ea
sergio <quetzal.mq97@gmail.com>
2024-10-10 11:32:30 -0600

    version 1.0.2

--- document/__manifest__.py
+++ document/__manifest__.py
@@ -5,7 +5,7 @@
     'summary':"""    Aplicacion para Documentos    """,
     'author': 'Sergio Martinez Meneses',
     'category': 'Informes',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'depends': ['base','base_setup','web','mail'],
     'data': [
         'security/ir.model.access.csv',
